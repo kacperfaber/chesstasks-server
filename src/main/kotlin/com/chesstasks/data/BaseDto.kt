90912369521150d335be58735ec0339058c9e257
Kacper Faber <kacperf1234@gmail.com>
2023-05-27 13:09:14 +0200

    Created BaseDto and BaseTable
    BaseTable - Simple wrapper for 'Table()', there's tableName passing. And there are two columns 'id' and 'createdAt'. Besides 'id' is set as primary key.
    
    BaseDto has the same two props and constructor passing from implementation to BaseDto constructor.

---
+++ src/main/kotlin/com/chesstasks/data/BaseDto.kt
@@ -0,0 +1,3 @@
+package com.chesstasks.data
+
+abstract class BaseDto(val id: Int, val createdAt: Long)
---
+++ src/main/kotlin/com/chesstasks/data/BaseTable.kt
@@ -0,0 +1,12 @@
+package com.chesstasks.data
+
+import org.jetbrains.exposed.sql.Table
+import kotlin.random.Random
+
+open class BaseTable(tableName: String) : Table(tableName) {
+    val id = integer("id").default(Random.nextInt())
+
+    val createdAt = long("created_at").default(System.currentTimeMillis())
+
+    override val primaryKey: PrimaryKey = PrimaryKey(id)
+}
