d268cd2fba3e4d1b1a481b071c5ff9c1ab4bff78
Kacper Faber <kacperf1234@gmail.com>
2023-05-27 16:25:13 +0200

    Added koin-annotations <3, Made UserService @Single.

--- src/main/kotlin/com/chesstasks/di/ConfigDi.kt
+++ src/main/kotlin/com/chesstasks/di/ConfigDi.kt
@@ -1,15 +1,17 @@
 package com.chesstasks.di
 
 import io.ktor.server.application.*
-import org.koin.dsl.module
+import org.koin.core.annotation.ComponentScan
+import org.koin.core.annotation.Module
+import org.koin.ksp.generated.module
 import org.koin.ktor.plugin.Koin
+
+@Module
+@ComponentScan(value = "com.chesstasks")
+class AppModule
 
 fun Application.configureDi() {
     install(Koin) {
-        val module = module {
-
-        }
-
-        modules()
+        modules(AppModule().module)
     }
 }
--- src/main/kotlin/com/chesstasks/services/user/UserService.kt
+++ src/main/kotlin/com/chesstasks/services/user/UserService.kt
@@ -2,7 +2,9 @@ package com.chesstasks.services.user
 
 import com.chesstasks.data.dao.UserDao
 import com.chesstasks.data.dto.UserDto
+import org.koin.core.annotation.Single
 
+@Single
 class UserService(private val userDao: UserDao) {
     suspend fun getById(id: Int): UserDto? = userDao.getById(id)
     suspend fun getByLogin(login: String): UserDto? = userDao.getByLogin(login)
