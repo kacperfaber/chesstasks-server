81fe8eacf0d852f1f5d45d870988a9e9495a7c1d
Kacper Faber <kacperf1234@gmail.com>
2023-05-28 12:53:41 +0200

    Modified ConfigDi
    1. Not prodModule and devModule binds implementation to interface...
    
    2. Created setupModules() to initialize the container. [To use in tests, because KOIN can't use the same container...]

--- src/main/kotlin/com/chesstasks/di/ConfigDi.kt
+++ src/main/kotlin/com/chesstasks/di/ConfigDi.kt
@@ -2,15 +2,16 @@ package com.chesstasks.di
 
 import com.chesstasks.Profiles
 import com.chesstasks.security.DevPasswordHasher
+import com.chesstasks.security.PasswordHasher
 import com.chesstasks.security.ProdPasswordHasher
-import com.chesstasks.services.token.DevTokenReader
-import com.chesstasks.services.token.DevTokenWriter
-import com.chesstasks.services.token.ProdTokenReader
-import com.chesstasks.services.token.ProdTokenWriter
+import com.chesstasks.services.token.*
 import com.google.gson.Gson
 import io.ktor.server.application.*
+import org.koin.core.KoinApplication
 import org.koin.core.annotation.ComponentScan
 import org.koin.core.annotation.Module
+import org.koin.core.context.startKoin
+import org.koin.dsl.binds
 import org.koin.dsl.module
 import org.koin.ksp.generated.module
 import org.koin.ktor.plugin.Koin
@@ -20,24 +21,30 @@ import org.koin.ktor.plugin.Koin
 class GlobalModule
 
 fun devModule() = module {
-    single { DevPasswordHasher() }
-    single { DevTokenReader(get()) }
-    single { DevTokenWriter(get()) }
+    single { DevPasswordHasher() } binds arrayOf(PasswordHasher::class)
+    single { DevTokenReader(get()) } binds arrayOf(TokenReader::class)
+    single { DevTokenWriter(get()) } binds arrayOf(TokenWriter::class)
 }
 
 fun prodModule() = module {
-    single { ProdPasswordHasher() }
-    single { ProdTokenReader() }
-    single { ProdTokenWriter() }
+    single { ProdPasswordHasher() } binds arrayOf(PasswordHasher::class)
+    single { ProdTokenReader() } binds arrayOf(TokenReader::class)
+    single { ProdTokenWriter() } binds arrayOf(TokenWriter::class)
 }
 
 fun gsonModule() = module {
     single { Gson() }
 }
 
+fun KoinApplication.setupModules() {
+    val profileModule = if (Profiles.isDev()) devModule() else prodModule()
+    modules(GlobalModule().module, profileModule, gsonModule())
+}
+
 fun Application.configureDi() {
-    install(Koin) {
-        val profileModule = if (Profiles.isDev()) devModule() else prodModule()
-        modules(GlobalModule().module, profileModule, gsonModule())
+    install(Koin) {}
+
+    startKoin {
+        setupModules()
     }
 }
