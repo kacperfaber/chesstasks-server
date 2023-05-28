d1ef960d0f43a37790bf3b7ee7e097c0e6da93b7
Kacper Faber <kacperf1234@gmail.com>
2023-05-28 13:11:39 +0200

    Created DevPasswordHasherTest, modified build.gradle.
    Created :devTest task to run tests with environment variable profile=dev

---
+++ src/test/kotlin/com/chesstasks/security/DevPasswordHasherTest.kt
@@ -0,0 +1,67 @@
+package com.chesstasks.security
+
+import com.chesstasks.di.setupModules
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.koin.core.context.GlobalContext.get
+import org.koin.core.context.startKoin
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+
+class DevPasswordHasherTest {
+    private val passwordHasher: PasswordHasher
+        get() = get().get()
+
+
+    @Test
+    fun `comparePasswords does not throw`() {
+        runBlocking {
+            assertDoesNotThrow { passwordHasher.comparePasswords("", "") }
+        }
+    }
+
+    @Test
+    fun `comparePasswords returns true if value equals`() {
+        runBlocking {
+            assertTrue { passwordHasher.comparePasswords("abc", "abc") }
+        }
+    }
+
+    @Test
+    fun `comparePasswords returns true if value and reference equals`() {
+        val abc = "abc"
+        runBlocking {
+            assertTrue { passwordHasher.comparePasswords(abc, abc) }
+        }
+    }
+
+    @Test
+    fun `comparePasswords returns true if value and reference doesn't equal`() {
+        runBlocking {
+            assertTrue { passwordHasher.comparePasswords("abc", "abc") }
+        }
+    }
+
+    @Test
+    fun `comparePasswords returns false if value doesn't equal`() {
+        runBlocking {
+            assertFalse{passwordHasher.comparePasswords("abc", "def")}
+            assertFalse{passwordHasher.comparePasswords("Kacper", "Faber")}
+            assertFalse{passwordHasher.comparePasswords("Test", "TEST")}
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        @BeforeAll
+        fun before() {
+            startKoin {
+                setupModules()
+            }
+        }
+    }
+}
