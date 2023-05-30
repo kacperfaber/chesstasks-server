9d7a9ff3014183fbd3dab84eddbd975ed7535beb
Kacper Faber <kacperf1234@gmail.com>
2023-05-30 13:32:42 +0200

    Added kotest, modified DatabaseFactory - added dropTables() method.

--- src/main/kotlin/com/chesstasks/data/DatabaseFactory.kt
+++ src/main/kotlin/com/chesstasks/data/DatabaseFactory.kt
@@ -5,10 +5,14 @@ import com.chesstasks.data.dto.Users
 import kotlinx.coroutines.Dispatchers
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import org.jetbrains.exposed.sql.transactions.transaction
+import javax.sql.DataSource
 
 object DatabaseFactory {
+    private val tables = listOf(Users, Tokens).toTypedArray()
+
     fun init() {
         // TODO: No 'production' database.
         val jdbc = "jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;CASE_INSENSITIVE_IDENTIFIERS=TRUE"
@@ -16,8 +20,24 @@ object DatabaseFactory {
         val db = Database.connect(jdbc, driverClassName, user = "sa", password = "sa")
 
         transaction(db) {
-            SchemaUtils.create(Users, Tokens)
+            SchemaUtils.create(*tables)
+
             commit()
+        }
+    }
+
+    fun init(dataSource: DataSource) {
+        val db = Database.connect(dataSource)
+
+        transaction(db) {
+            SchemaUtils.create(*tables)
+            commit()
+        }
+    }
+
+    fun dropTables() {
+        transaction {
+            SchemaUtils.drop(*tables)
         }
     }
 
