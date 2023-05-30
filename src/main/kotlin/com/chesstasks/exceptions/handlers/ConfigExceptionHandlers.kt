75e913e1a7ce2543e613941e3aa10d7194ef1918
Kacper Faber <kacperf1234@gmail.com>
2023-05-30 18:10:06 +0200

    ConfigExceptionHandlers.kt - added ForbidException.

--- src/main/kotlin/com/chesstasks/exceptions/handlers/ConfigExceptionHandlers.kt
+++ src/main/kotlin/com/chesstasks/exceptions/handlers/ConfigExceptionHandlers.kt
@@ -1,5 +1,6 @@
 package com.chesstasks.exceptions.handlers
 
+import com.chesstasks.security.auth.ForbidException
 import com.chesstasks.security.auth.UnauthorizedException
 import io.ktor.http.*
 import io.ktor.server.application.*
@@ -8,8 +9,12 @@ import io.ktor.server.response.*
 
 fun Application.configureExceptionHandlers() {
     install(StatusPages) {
-        exception<UnauthorizedException> {
-            call, exception -> call.respond(HttpStatusCode.Unauthorized, ExceptionResponse(exception.message))
+        exception<UnauthorizedException> { call, exception ->
+            call.respond(HttpStatusCode.Unauthorized, ExceptionResponse(exception.message))
+        }
+
+        exception<ForbidException> { call, exception ->
+            call.respond(HttpStatusCode.Forbidden, ExceptionResponse(exception.message))
         }
     }
 }
