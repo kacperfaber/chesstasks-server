6262422dc2388d0017a363547b09c9530095fbd5
Kacper Faber <kacperf1234@gmail.com>
2023-06-01 11:16:18 +0200

    Created ApplicationCall.ofNullable(T?), it will respond bad request if null.

---
+++ src/main/kotlin/com/chesstasks/controllers/CallExtensions.kt
@@ -0,0 +1,10 @@
+package com.chesstasks.controllers
+
+import com.chesstasks.exceptions.BadRequestException
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+
+suspend inline fun <reified T> ApplicationCall.ofNullable(value: T?) {
+    respond(HttpStatusCode.OK, value ?: throw BadRequestException())
+}
