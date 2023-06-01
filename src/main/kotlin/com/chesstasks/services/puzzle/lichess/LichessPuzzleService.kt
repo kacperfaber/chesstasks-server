51d387e1556ba1a8ed29ccb43a03d7f6ccbf5556
Kacper Faber <kacperf1234@gmail.com>
2023-06-01 10:22:37 +0200

    Created LichessPuzzleService (getById).

---
+++ src/main/kotlin/com/chesstasks/services/puzzle/lichess/LichessPuzzleService.kt
@@ -0,0 +1,12 @@
+package com.chesstasks.services.puzzle.lichess
+
+import com.chesstasks.data.dao.LichessPuzzleDao
+import com.chesstasks.data.dto.LichessPuzzleDto
+import org.koin.core.annotation.Single
+
+@Single
+class LichessPuzzleService(val lichessPuzzleDao: LichessPuzzleDao) {
+    suspend fun getById(id: String): LichessPuzzleDto? {
+        return lichessPuzzleDao.getById(id)
+    }
+}
