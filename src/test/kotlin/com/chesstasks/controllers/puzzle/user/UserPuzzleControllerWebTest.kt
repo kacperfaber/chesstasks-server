f14b7d34681e456ff49349d9c231bc23558e3cc5
Kacper Faber <kacperf1234@gmail.com>
2023-06-07 15:27:28 +0200

    Tested endpoint DEL /user-puzzle/as-owner/:id

--- src/test/kotlin/com/chesstasks/controllers/puzzle/user/UserPuzzleControllerWebTest.kt
+++ src/test/kotlin/com/chesstasks/controllers/puzzle/user/UserPuzzleControllerWebTest.kt
@@ -1,15 +1,21 @@
 package com.chesstasks.controllers.puzzle.user
 
 import com.chesstasks.data.dto.Admins
+import com.chesstasks.data.dto.UserPuzzleDto
 import com.chesstasks.data.dto.UserPuzzles
 import com.chesstasks.data.dto.Users
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.test.dispatcher.*
 import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.junit.jupiter.api.Test
 import testutils.*
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class UserPuzzleControllerWebTest : BaseWebTest() {
     private fun setupUser() {
@@ -19,6 +25,17 @@ class UserPuzzleControllerWebTest : BaseWebTest() {
                 it[emailAddress] = "kacperf1234@gmail.com"
                 it[passwordHash] = "HelloWorld123"
                 it[username] = "kacperfaber"
+            }
+        }
+    }
+
+    private fun setupSecondUser() {
+        transaction {
+            Users.insert {
+                it[Users.id] = 1
+                it[emailAddress] = "teresajen@gmail.com"
+                it[passwordHash] = "HelloWorld123"
+                it[username] = "teresajen"
             }
         }
     }
@@ -41,6 +58,12 @@ class UserPuzzleControllerWebTest : BaseWebTest() {
                 it[ranking] = 1500
                 it[ownerId] = 0
             }
+        }
+    }
+
+    private fun userPuzzleLen(): Long {
+        return transaction {
+            UserPuzzles.selectAll().count()
         }
     }
 
@@ -78,5 +101,79 @@ class UserPuzzleControllerWebTest : BaseWebTest() {
         response.jsonPath("$.fen", "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
         response.jsonPath("$.moves", "e2e4")
         response.jsonPath("$.ranking", 1500)
+    }
+
+    @Test
+    fun `deleteAsOwnerEndpoint returns FORBIDDEN if no authentication`() = testSuspend {
+        setupUser()
+        setupUserPuzzle()
+
+        app.client.delete("/user-puzzle/as-owner/0").status.isForbid()
+    }
+
+    @Test
+    fun `deleteAsOwnerEndpoint returns BAD_REQUEST if puzzle doesnt exist and authenticated`() = testSuspend {
+        setupUser()
+        app.client.delete("/user-puzzle/as-owner/0"){withToken(0)}.status.isBadRequest()
+    }
+
+    @Test
+    fun `deleteAsOwnerEndpoint returns BAD_REQUEST if puzzle exist but authenticated as not owner`() = testSuspend {
+        setupUser()
+        setupSecondUser()
+        setupUserPuzzle()
+        app.client.delete("/user-puzzle/as-owner/0"){withToken(1)}.status.isBadRequest()
+    }
+
+    @Test
+    fun `deleteAsOwnerEndpoint returns NO_CONTENT if puzzle exist and authenticated as owner`() = testSuspend {
+        setupUser()
+        setupUserPuzzle()
+        app.client.delete("/user-puzzle/as-owner/0"){withToken(0)}.status.isNoContent()
+    }
+
+    @Test
+    fun `deleteAsOwnerEndpoint makes UserPuzzle table shorter by 1 and returns NO_CONTENT`() = testSuspend {
+        setupUser()
+        setupUserPuzzle()
+        val before = userPuzzleLen()
+        app.client.delete("/user-puzzle/as-owner/0"){withToken(0)}.status.isNoContent()
+        val now = userPuzzleLen()
+        assertEquals(before - 1, now)
+    }
+
+    private fun userPuzzleHasItem(id: Int): Boolean {
+        return transaction {
+            UserPuzzles.select { UserPuzzles.id eq id }.map { UserPuzzleDto::from }.any()
+        }
+    }
+
+    @Test
+    fun `deleteAsOwnerEndpoint deletes record from db and returns NO_CONTENT`() = testSuspend {
+        setupUser()
+        setupUserPuzzle()
+        assertTrue { userPuzzleHasItem(0) }
+        app.client.delete("/user-puzzle/as-owner/0"){withToken(0)}.status.isNoContent()
+        assertFalse { userPuzzleHasItem(0) }
+    }
+
+    @Test
+    fun `deleteAsOwnerEndpoint doesnt affect records length when returns FORBIDDEN`() = testSuspend {
+        setupUser()
+        setupUserPuzzle()
+        val before = userPuzzleLen()
+        app.client.delete("/user-puzzle/as-owner/0").status.isForbid()
+        val now = userPuzzleLen()
+        assertEquals(before, now)
+    }
+
+    @Test
+    fun `deleteAsOwnerEndpoint doesnt affect records length when returns BAD_REQUEST`() = testSuspend {
+        setupUser()
+        setupUserPuzzle()
+        val before = userPuzzleLen()
+        app.client.delete("/user-puzzle/as-owner/1"){withToken(0)}.status.isBadRequest()
+        val now = userPuzzleLen()
+        assertEquals(before, now)
     }
 }
