1ff5dce838b60776ee501b1d715682b8f15b84f1
Kacper Faber <kacperf1234@gmail.com>
2023-06-07 14:26:19 +0200

    Created ApplicationCall.requirePrincipalId
    Returns current user id or throws an exception.

--- src/main/kotlin/com/chesstasks/controllers/CallExtensions.kt
+++ src/main/kotlin/com/chesstasks/controllers/CallExtensions.kt
@@ -1,8 +1,10 @@
 package com.chesstasks.controllers
 
 import com.chesstasks.exceptions.BadRequestException
+import com.chesstasks.security.auth.TokenPrincipal
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
 import io.ktor.server.response.*
 
 suspend inline fun <reified T> ApplicationCall.ofNullable(value: T?) {
@@ -11,4 +13,8 @@ suspend inline fun <reified T> ApplicationCall.ofNullable(value: T?) {
 
 suspend fun ApplicationCall.ofBoolean(value: Boolean, whenTrue: HttpStatusCode = HttpStatusCode.NoContent) {
     respond(if (value) whenTrue else HttpStatusCode.BadRequest)
+}
+
+fun ApplicationCall.requirePrincipalId(): Int {
+    return principal<TokenPrincipal>()?.user?.id ?: throw Exception("No principal")
 }
