40c5b8e1dee40d2177d20392d442f18e219cbab8
Kacper Faber <kacperf1234@gmail.com>
2023-06-08 14:43:51 +0200

    Added ConfigUI to ControllersConfig.kt

--- src/main/kotlin/com/chesstasks/controllers/ControllersConfig.kt
+++ src/main/kotlin/com/chesstasks/controllers/ControllersConfig.kt
@@ -3,6 +3,7 @@ package com.chesstasks.controllers
 import com.chesstasks.controllers.authentication.authenticationController
 import com.chesstasks.controllers.puzzle.lichess.lichessPuzzleController
 import com.chesstasks.controllers.puzzle.user.userPuzzleController
+import com.chesstasks.controllers.ui.configUI
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 
@@ -11,5 +12,11 @@ fun Application.configureControllers() {
         authenticationController()
         lichessPuzzleController()
         userPuzzleController()
+
+        // TODO: Embrace this 3 lines above in one 'configUI'
+
+        // TODO: Set prefix on api's endpoint to api/
+
+        configUI()
     }
 }
---
+++ src/main/kotlin/com/chesstasks/controllers/ui/ConfigUI.kt
@@ -0,0 +1,11 @@
+package com.chesstasks.controllers.ui
+
+import io.ktor.server.routing.*
+
+fun Route.configUI() {
+    // TODO: Add controllers for both (admin and user)
+
+    // TODO: Add admin controllers here.
+
+    // TODO: Add user controllers here.
+}
