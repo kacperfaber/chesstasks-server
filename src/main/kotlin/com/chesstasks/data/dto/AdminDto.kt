8340ff57581652b7671616386374ce7259437a45
Kacper Faber <kacperf1234@gmail.com>
2023-06-13 17:54:42 +0200

    AdminDto is extending BaseDto.

--- src/main/kotlin/com/chesstasks/data/dto/AdminDto.kt
+++ src/main/kotlin/com/chesstasks/data/dto/AdminDto.kt
@@ -1,9 +1,10 @@
 package com.chesstasks.data.dto
 
+import com.chesstasks.data.BaseDto
 import com.chesstasks.data.BaseTable
 
 object Admins : BaseTable("admin") {
     val userId = integer("user_id").references(Users.id)
 }
 
-data class AdminDto(val id: Int, val createdAt: Long, val userId: Int)
+class AdminDto(id: Int, createdAt: Long, val userId: Int) : BaseDto(id, createdAt)
