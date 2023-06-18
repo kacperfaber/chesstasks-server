83ff69e0e0fae08280bd6053bfa5caa25ca26fe5
Kacper Faber <kacperf1234@gmail.com>
2023-06-18 14:12:30 +0200

    In Handlers.kt created Actions Security.
    It's the system when registering handlers.
    
    We're using it like this.
    
    {
    @this=Handlers.Config
    handle("test") {
    // no security
    }
    
    user {
        handle("hello") {
        // only users [and admins in this sample].
        }
    }
    }
    }

--- src/main/kotlin/com/chesstasks/websocket/ConfigWebSocket.kt
+++ src/main/kotlin/com/chesstasks/websocket/ConfigWebSocket.kt
@@ -1,7 +1,12 @@
 package com.chesstasks.websocket
 
+import com.chesstasks.security.auth.TokenPrincipal
+import com.chesstasks.websocket.handlers.*
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
+import io.ktor.websocket.*
 import java.time.Duration
 
 fun Application.configWebSocket() {
@@ -12,5 +17,29 @@ fun Application.configWebSocket() {
         timeout = Duration.ofSeconds(15)
         maxFrameSize = Long.MAX_VALUE
         masking = false
+    }
+
+    configHandlers {
+        handle("test") {
+            send("Test passed <3")
+        }
+
+        user {
+            handle("curr") {
+                send("current_user", call.principal<TokenPrincipal>()?.user?.id ?: "null")
+            }
+        }
+    }
+
+    routing {
+        webSocket("/test") {
+            for (frame in incoming) {
+                val msg = String(frame.readBytes())
+                val msgSplit = msg.split(" ")
+                val handler: Handler = Handlers.handlers.filter { it.key == msgSplit[0] }.values.first()
+                handler.validate(this)
+                handler.onReceived(this, Command(msgSplit[0], msgSplit.getOrElse(1) {"null"}))
+            }
+        }
     }
 }
