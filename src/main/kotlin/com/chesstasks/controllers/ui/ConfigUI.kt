e15881c7bf763374320f6d6579cb10be78a77f15
Kacper Faber <kacperf1234@gmail.com>
2023-06-21 13:23:05 +0200

    Serving static content.

--- src/main/kotlin/com/chesstasks/controllers/ui/ConfigUI.kt
+++ src/main/kotlin/com/chesstasks/controllers/ui/ConfigUI.kt
@@ -1,11 +1,24 @@
 package com.chesstasks.controllers.ui
 
+import com.chesstasks.controllers.view
+import io.ktor.server.application.*
+import io.ktor.server.http.content.*
 import io.ktor.server.routing.*
+import org.koin.ktor.ext.get
+import java.io.File
 
 fun Route.configUI() {
+    staticFiles("/public", File("src/main/resources/public")) {
+        default("/css/style.css")
+    }
+
     // TODO: Add controllers for both (admin and user)
 
     // TODO: Add admin controllers here.
 
     // TODO: Add user controllers here.
+
+    get("/ui") {
+        call.view("home/home")
+    }
 }
