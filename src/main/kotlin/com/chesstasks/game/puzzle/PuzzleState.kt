d043e3c195b38a67bf355301b8fcd3b992e7ea21
Kacper Faber <kacperf1234@gmail.com>
2023-06-24 17:08:52 +0200

    PuzzleState.kt
    Created some stuff, index is 0 by default.

--- src/main/kotlin/com/chesstasks/game/puzzle/PuzzleState.kt
+++ src/main/kotlin/com/chesstasks/game/puzzle/PuzzleState.kt
@@ -1,10 +1,34 @@
 package com.chesstasks.game.puzzle
 
 class PuzzleState(val puzzleId: Int, val fen: String, val computerMoves: List<BaseMove>, val userMoves: List<Move>) {
-    var index = 1
+    var index = 0
 
     val firstMove: String
         get() = computerMoves[0].move
+
+    val userMove: Move
+        get() = userMoves[index]
+
+    val computerMove: BaseMove
+        get() = computerMoves[index]
+
+    fun submit(state: MoveSolveState) {
+        userMove.solve = state
+        index += 1
+    }
+
+    enum class FinishState(val value: String) {
+        Solved("solved"),
+        Wrong("wrong")
+    }
+
+    val finishState: FinishState?
+        get() {
+            if (userMoves.all { it.solve == MoveSolveState.Ok }) return FinishState.Solved
+            if (userMoves.any { it.solve == MoveSolveState.Wrong }) return FinishState.Wrong
+            return null
+        }
+
 }
 
 open class BaseMove(val move: String)
@@ -14,4 +38,4 @@ enum class MoveSolveState(val value: String) {
     Wrong("wrong")
 }
 
-class Move(move: String, val solve: MoveSolveState?) : BaseMove(move)
+class Move(move: String, var solve: MoveSolveState?) : BaseMove(move)
