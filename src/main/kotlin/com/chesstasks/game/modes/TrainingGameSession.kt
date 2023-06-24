6f05c503ecacbb59f35c0e59342bc026ed78d362
Kacper Faber <kacperf1234@gmail.com>
2023-06-24 17:33:07 +0200

    Modified TrainingGameSession.

--- src/main/kotlin/com/chesstasks/game/modes/TrainingGameSession.kt
+++ src/main/kotlin/com/chesstasks/game/modes/TrainingGameSession.kt
@@ -2,12 +2,18 @@ package com.chesstasks.game.modes
 
 import com.chesstasks.data.dto.UserDto
 import com.chesstasks.game.GameSession
+import com.chesstasks.game.GameSessions
 import io.ktor.server.websocket.*
 
-class TrainingGameSession(user: UserDto, val rankingDifference: Int, val themeId: Int?, session: DefaultWebSocketServerSession) : GameSession(user, session) {
-    companion object {
-        fun new(user: UserDto, rankingDifference: Int, themeId: Int?, session: DefaultWebSocketServerSession): TrainingGameSession {
-            return TrainingGameSession(user, rankingDifference, themeId, session)
-        }
-    }
+class TrainingGameSession(user: UserDto, val settings: Settings, session: DefaultWebSocketServerSession) :
+    GameSession(user, session) {
+    data class Settings(val rankingOffset: Int = 0)
+}
+
+fun GameSessions.startTrainingSession(
+    user: UserDto,
+    settings: TrainingGameSession.Settings,
+    session: DefaultWebSocketServerSession): TrainingGameSession
+{
+    return TrainingGameSession(user, settings, session).apply { start(this) }
 }
