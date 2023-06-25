2c9148babbcdcd3b58f3705138f6bf9ae5b892af
Kacper Faber <kacperf1234@gmail.com>
2023-06-25 13:56:06 +0200

    Created not implemented RankingCalculator.

---
+++ src/main/kotlin/com/chesstasks/services/training/ranking/RankingCalculator.kt
@@ -0,0 +1,10 @@
+package com.chesstasks.services.training.ranking
+
+import org.koin.core.annotation.Single
+
+@Single
+class RankingCalculator {
+    fun getNewRanking(ranking: Int, puzzleRanking: Int) {
+        TODO()
+    }
+}
