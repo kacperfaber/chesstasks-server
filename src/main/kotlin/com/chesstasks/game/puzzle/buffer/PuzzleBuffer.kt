5edf5fc56da5e51849b8c718f155838f28765e14
Kacper Faber <kacperf1234@gmail.com>
2023-06-25 14:44:22 +0200

    Created PuzzleBuffer and PuzzleBufferController.

---
+++ src/main/kotlin/com/chesstasks/game/puzzle/buffer/PuzzleBuffer.kt
@@ -0,0 +1,9 @@
+package com.chesstasks.game.puzzle.buffer
+
+import com.chesstasks.data.dto.PuzzleDto
+
+data class PuzzleBuffer(val userId: Int, val settings: Settings) {
+    internal var puzzles: List<PuzzleDto> = mutableListOf()
+
+    data class Settings(val rankingMin: Int, val rankingMax: Int)
+}
