52279aec4de9cf34727b959ee26496eb6824dc6a
Kacper Faber <kacperf1234@gmail.com>
2023-08-11 12:51:36 +0200

    Fast impl of RankingCalculator:
    - the pattern is
    diff = puzzle_rank - user_rank
    inc = if(success) 1 else -1
    new_user_rank = diff * inc * 0.01

--- src/main/kotlin/com/chesstasks/services/training/ranking/RankingCalculator.kt
+++ src/main/kotlin/com/chesstasks/services/training/ranking/RankingCalculator.kt
@@ -1,10 +1,15 @@
 package com.chesstasks.services.training.ranking
 
 import org.koin.core.annotation.Single
+import kotlin.math.roundToInt
 
 @Single
 class RankingCalculator {
     fun getNewRanking(ranking: Int, puzzleRanking: Int, success: Boolean): Int {
-        TODO()
+        // TODO: (RankingCalculator) It doesn't work like I want this to work...
+
+        val inc = if (success) 1 else -1
+        val diff = puzzleRanking - ranking
+        return ((diff * inc) * 0.01).roundToInt()
     }
 }
