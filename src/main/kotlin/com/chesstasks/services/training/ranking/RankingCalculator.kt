e975ab45649e09fca1dc41ca154fb16ee7686c80
Kacper Faber <kacperf1234@gmail.com>
2023-08-28 10:47:41 +0200

    Better RankingCalculator - still there' something to work.

--- src/main/kotlin/com/chesstasks/services/training/ranking/RankingCalculator.kt
+++ src/main/kotlin/com/chesstasks/services/training/ranking/RankingCalculator.kt
@@ -1,6 +1,7 @@
 package com.chesstasks.services.training.ranking
 
 import org.koin.core.annotation.Single
+import kotlin.math.abs
 import kotlin.math.roundToInt
 
 @Single
@@ -8,8 +9,48 @@ class RankingCalculator {
     fun getNewRanking(ranking: Int, puzzleRanking: Int, success: Boolean): Int {
         // TODO: (RankingCalculator) It doesn't work like I want this to work...
 
-        val inc = if (success) 1 else -1
         val diff = puzzleRanking - ranking
-        return ((diff * inc) * 0.01).roundToInt()
+        val diffAbs = abs(diff)
+
+        if (success) {
+            if (diff > 0) {
+                // puzzle 1500
+                // user 1000
+                // diff = 500
+                // diffAbs = 500
+                // 1500 + (500 * .05) = 1525
+                return ranking + (diffAbs * 0.05).roundToInt()
+            }
+
+            else {
+                // puzzle 1500
+                // user 2000
+                // diff = -500
+                // diffAbs = 500
+                // 1500 + (500 * .02) = 1510
+                return ranking + (diffAbs * 0.02).roundToInt()
+            }
+        }
+
+        else {
+            if (diff < 0) {
+                // puzzle 1500
+                // user 2000
+                // diff = -500
+                // diffAbs = 500
+                // 1500 + (500 * -0.05) = 1475
+
+                return ranking + (diffAbs * -0.05).roundToInt()
+            }
+
+            else {
+                // puzzle 1500
+                // user 1000
+                // diff = 500
+                // diffAbs = 500
+                // 1500 + (500 * -0.01) = 1495
+                return ranking + (diffAbs * -0.01).roundToInt()
+            }
+        }
     }
 }
