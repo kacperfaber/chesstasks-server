348aa6d8f78343defc27f6f3c5674f9bafc9bee3
Kacper Faber <kacperf1234@gmail.com>
2023-08-28 15:01:49 +0200

    Fixed RankingCalculator...
    When the diff was 0, the ranking will be set to the same (ranking + 0 * i).

--- src/main/kotlin/com/chesstasks/services/training/ranking/RankingCalculator.kt
+++ src/main/kotlin/com/chesstasks/services/training/ranking/RankingCalculator.kt
@@ -2,6 +2,7 @@ package com.chesstasks.services.training.ranking
 
 import org.koin.core.annotation.Single
 import kotlin.math.abs
+import kotlin.math.ceil
 import kotlin.math.roundToInt
 
 @Single
@@ -10,7 +11,7 @@ class RankingCalculator {
         // TODO: (RankingCalculator) It doesn't work like I want this to work...
 
         val diff = puzzleRanking - ranking
-        val diffAbs = abs(diff)
+        val diffAbs = if (diff != 0) abs(diff) else 5
 
         if (success) {
             if (diff > 0) {
@@ -19,7 +20,7 @@ class RankingCalculator {
                 // diff = 500
                 // diffAbs = 500
                 // 1500 + (500 * .05) = 1525
-                return ranking + (diffAbs * 0.05).roundToInt()
+                return ranking + (ceil(diffAbs * 0.05).toInt())
             }
 
             else {
@@ -28,7 +29,7 @@ class RankingCalculator {
                 // diff = -500
                 // diffAbs = 500
                 // 1500 + (500 * .02) = 1510
-                return ranking + (diffAbs * 0.02).roundToInt()
+                return ranking + ceil((diffAbs * 0.02)).toInt()
             }
         }
 
@@ -40,7 +41,7 @@ class RankingCalculator {
                 // diffAbs = 500
                 // 1500 + (500 * -0.05) = 1475
 
-                return ranking + (diffAbs * -0.05).roundToInt()
+                return ranking + (ceil(diffAbs * 0.05) * -1).toInt()
             }
 
             else {
@@ -49,7 +50,7 @@ class RankingCalculator {
                 // diff = 500
                 // diffAbs = 500
                 // 1500 + (500 * -0.01) = 1495
-                return ranking + (diffAbs * -0.01).roundToInt()
+                return ranking + (ceil((diffAbs * 0.01)) * -1).toInt()
             }
         }
     }
